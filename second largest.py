523881641518c6bb2e425fa4890f071e813f871d
Shivangi Singhal <shivangisinghal1995@gmail.com>
2016-04-02 16:12:18 +0530

    Find the second largest number in the list

--- second largest.py
+++ second largest.py
@@ -1,6 +1,10 @@
-N= print(input("How many numbers do you want to add in the list?"))
+N= input("How many numbers do you want to add in the list?")
 print("Enter Numbers:")
 mylist=list(map(int,input().split()))
-mylist.sort()
-mylist.reverse()
+for n in mylist:
+    if n > mylist[0]:
+        mylist[0],mylist[1]=n,mylist[0]
+    elif mylist[0]>n>mylist[1]:
+        mylist[1]=n
+print("The Second largest number is:")
 print(mylist[1])
